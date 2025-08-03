d0328c53d6aadfd742dbd524e14db54bfb3ec4cd
razaulkarim2410 <razaulkarim2410@gmail.com>
2025-08-03 17:21:36 +0600

    added resume view and download option

--- src/components/heroSection/HeroText.jsx
+++ src/components/heroSection/HeroText.jsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import { fadeIn } from "../../framerMotion/variants";
+import { FileDown } from "lucide-react";
 
 const HeroText = () => {
   return (
@@ -34,6 +35,20 @@ const HeroText = () => {
         A Passionate Web Developer  <br /> with 8 Months of
         Experience.
       </motion.p>
+
+       {/* Resume Button with Icon */}
+      <motion.a
+        href="/Razaul_Karim_Resume.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+        variants={fadeIn("up", 0.8)}
+        initial="hidden"
+        whileInView="show"
+        className="inline-flex items-center justify-center gap-2 mt-6 px-6 py-3 bg-cyan hover:bg-darkCyan text-white rounded-lg text-lg font-semibold transition duration-300"
+      >
+        <FileDown className="w-5 h-5" />
+        View & Download Resume
+      </motion.a>
     </div>
   );
 };
